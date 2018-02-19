7daa85f0e861af0e0bdf063435d0ed0dbe5e3ef6
Dorsey Dick <dorseydick@gmail.com>
2018-02-19 13:21:13 -0500

    Wire up employee create

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 
 const RouterComponent = () => {
@@ -12,7 +13,15 @@ const RouterComponent = () => {
           <Scene key="login" component={LoginForm} title="Please Login" initial />
         </Scene>
         <Scene key="main">
-          <Scene key="employeeList" component={EmployeeList} title="Employees" />
+          <Scene
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            rightTitle="Add"
+            onRight={() => Actions.employeeCreate()}
+            initial
+          />
+          <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
         </Scene>
       </Scene>
     </Router>
